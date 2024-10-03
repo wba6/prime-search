f748c36b8cebb135ab4ab751518549f2e4af84c5
William  A. <76547127+wba6@users.noreply.github.com>
2024-10-03 00:06:27 -0400

    update algorithm

--- code/src/stats.rs
+++
@@ -1,8 +0,0 @@
-use std::time::Duration;
-
-pub struct AlgorithmStats {
-    pub algorithm_name: String,
-    pub time_taken: Duration,
-    pub largest_prime: u64,
-    pub num_primes_found: usize,
-}
